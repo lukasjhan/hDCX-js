bdbeca2b30065628cce05fe2a1319212799ceb1d
Seohee Park <dvlprsh103@gmail.com>
2025-06-02 21:52:34 +0900

    chore: setup initial project configuration (#4)

---
+++ packages/core/src/test/index.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from 'vitest';
+
+describe('Core package', () => {
+  test('should pass initial test', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ packages/expo-secure-store/src/test/index.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from 'vitest';
+
+describe('Expo Secure Store package', () => {
+  test('should pass initial test', () => {
+    expect(true).toBe(true);
+  });
+});
