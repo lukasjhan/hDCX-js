c7f8f2ec17f8852cc59ed29b910f8f1b658ef9b4
Lukas.J.Han <lukas@hopae.io>
2025-06-08 16:13:37 +0900

    feat: add OID4VCI client integration for credential issuance (#14)

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,14 +1,36 @@
 import CredentialStore from '../credentials/credential-store';
 import { InMemoryStorage } from '../storage';
 import { ICredentialStore, IWalletStorage } from '../types/storage';
+import { Oid4VciClient } from '@vdcs/oid4vci-client';
+import { type EcPrivateJwk } from '@vdcs/jwt';
+import { type CredentialResponse } from '@vdcs/oid4vci';
 
 class WalletSDK {
   credentialStore: ICredentialStore;
+  jwk: EcPrivateJwk;
 
-  constructor({ storage }: { storage: IWalletStorage }) {
+  constructor({
+    storage,
+    jwk,
+  }: {
+    storage: IWalletStorage;
+    jwk: EcPrivateJwk;
+  }) {
     this.credentialStore = new CredentialStore(
       storage || new InMemoryStorage(),
     );
+    this.jwk = jwk;
+  }
+
+  async request(offerUri: string): Promise<CredentialResponse> {
+    const client = await Oid4VciClient.fromOfferUrl(offerUri);
+    const credential = await client.getCredential({
+      credential_configuration_id:
+        client.credentialOffer.credential_configuration_ids[0],
+      privateKey: this.jwk,
+    });
+
+    return credential;
   }
 }
 
