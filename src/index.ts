7438a2df27f0589a5b9c97ebd926eb57a812a7fc
Lukas.J.Han <lukas@hopae.io>
2025-06-24 06:38:39 +0900

    feat: add types directory and export shared type definitions (#34)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './core';
 export * from './storage';
 export * from './expo';
+export * from './types';
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export * from './storage';
+export * from './credential';
+export type { rawDCQL, DCQL } from '@vdcs/dcql';
+export type { CredentialResponse } from '@vdcs/oid4vci';
+export type { RequestObject } from '@vdcs/oid4vp-client';
+export type { PresentationFrame } from '@sd-jwt/types';
+export type { EcPrivateJwk } from '@vdcs/jwt';
